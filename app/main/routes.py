537ba03d2deb283cd3832ac3de3e917290061b87
8kyu <guillemsenabre@gmail.com>
2024-02-20 19:31:25 +0100

    FEAT: fetch working

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,3 +6,7 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/')
 def index():
   return render_template('index.html', result=None)
+
+@main_bp.route('/getTitanicView')
+def get_titanic():
+  return render_template('titanic.html', result=None)
